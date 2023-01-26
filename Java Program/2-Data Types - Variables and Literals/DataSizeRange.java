425cd560bb03805457c7772fa2bc6b5e0fe19e4c
Anjali Bharti <81773990+anjalibharti5@users.noreply.github.com>
2023-01-26 16:06:20 +0530

    Add files via upload

---
+++ Java Program/2-Data Types - Variables and Literals/DataSizeRange.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+public class DataSizeRange {
+    public static void main(String arg[])
+    {
+        System.out.println("Int Min :" + Integer.MIN_VALUE);
+        System.out.println("Int Min :" + Integer.MAX_VALUE);
+        System.out.println("Int Min :" + Integer.BYTES);
+    }
+}
